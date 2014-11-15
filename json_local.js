550f3347a14a3d9729b8fe1ff411d8c824c6a77a
Syeda Kauser Inamdar <skinamdar@ucdavis.edu>
2014-11-15 12:56:08 -0800

    modified json

--- json_local.js
+++ json_local.js
@@ -4,19 +4,30 @@ $("#submitbutton").click(function () {
    //alert("saved!");
     if (window.localStorage) {
         var story = {};
+        //debugger;
         story.date = new Date();
         //alert(story.date);
         story.user = $("#usernameid").val();
        //alert(story.user);
         story.singlestory = $("#storyid").val(); // blank from text ; //create id for storie text 
-        alert(story.singlestory);
-        var stories = localStorage.getItem("stories") || [];
+        //alert(story.singlestory);
+        try{
+            var stories = JSON.parse(localStorage.getItem("stories"));
+            if (typeof stories !== "object" ) {
+             stories = []; 
+            }
+        } catch(err) {
+            stories = [];
+        }
         stories.push(story);
-        localStorage.setItem("stories",stories); // maybe stringify 
+        localStorage.setItem("stories",JSON.stringify(stories)); // maybe stringify 
         alert("The data has been stored successfully!");
     } else {
         alert("Your Browser does not support LocalStorage.");
     }
+    //return false;
+
+    window.location = "viewposts.html";
 });
 
 /*
