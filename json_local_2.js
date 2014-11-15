e8b12f7dd57a829d010e29518ffa4ef8a24fed53
Syeda Kauser Inamdar <skinamdar@ucdavis.edu>
2014-11-15 10:15:56 -0800

    First Attempt at Json Parsing. Modifying get and read functions

---
+++ json_local_2.js
@@ -0,0 +1,28 @@
+
+
+function saveLocal(){
+    if (window.localStorage) {
+        var story = {};
+        story.datae = new Date();
+        story.user = "A"; // create id for document.getelement.id.value
+        story.story = ""; // blank from text ; //create id for storie text 
+        var stories = localStorage.getItem("stories") || [];
+        stories.push(story);
+
+        localStorage.setItem("stories",stories); // maybe stringify 
+        alert("The data has been stored successfully.");
+    } else {
+        alert("Your Browser does not support LocalStorage.");
+    }
+}
+
+function readLocal(){
+    if (window.localStorage) {
+        //get the highscores object
+        var scores = localStorage.getItem("highscores");
+        scores = JSON.parse(scores);
+        for (i=0;i<scores.length;i++){
+            alert(scores[i]);
+        }
+    }
+}
